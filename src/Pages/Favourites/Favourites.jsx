40c447fe88fa8365c51dce7aa837caa33ba73f75
rifat3790 <mdrifayethossen@gmail.com>
2023-09-24 00:11:10 +0600

    added total price and rating

--- src/Pages/Favourites/Favourites.jsx
+++ src/Pages/Favourites/Favourites.jsx
@@ -9,18 +9,25 @@ const Favourites = () => {
 
     const [noFound, setNoFound] = useState(false);
 
+    const [isShow, setIsShow] = useState(false);
+
+    const [totalPrice, setTotalPrice] = useState(0);
+
     
     useEffect(()=>{
         const favorItems = JSON.parse(localStorage.getItem("favorites"));
         if(favorItems){
             setFavorites(favorItems);
+            const total = favorItems.reduce((preValue, currentItem)=> preValue + currentItem.price, 0 )
+            setTotalPrice(total);
         }
         else{
             setNoFound("No data found");
         }
     },[])
 
-    console.log(favorites);
+    // console.log(isShow);
+    // console.log(favorites);
 
     const handleRemove = () => {
         localStorage.clear();
@@ -31,6 +38,7 @@ const Favourites = () => {
     return (
         <div>
             <h1>Favorites items: {favorites.length}</h1>
+            <h3 className="text-xl text-center my-5 font-semibold">Total Price: {totalPrice.toFixed(2)}</h3>
             {noFound? <p className="h-[80vh] flex justify-center items-center">{noFound}</p>: <div>
 
                 {favorites.length > 0 && <div className="flex justify-center items-center"><button onClick={handleRemove} className="btn btn-secondary">Delete All Favorites</button></div>}
@@ -38,11 +46,11 @@ const Favourites = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 py-10">
                  {
-                    favorites.map(phone => <FavouritesCard key={phone.id} phone={phone}></FavouritesCard>)
+                    isShow ? favorites.map(phone => <FavouritesCard key={phone.id} phone={phone}></FavouritesCard>) : favorites.slice(0,4).map(phone => <FavouritesCard key={phone.id} phone={phone}></FavouritesCard>)
                  }
                 </div>
 
-                <div className="flex justify-center items-center"><button onClick={handleRemove} className="btn btn-secondary">Delete All Favorites</button></div>
+                {favorites.length >4 ? <div className="flex justify-center items-center"><button onClick={()=> setIsShow(!isShow)} className="btn btn-primary">{isShow? <p>Show Less</p>: <p>Show More</p>}</button></div> : ""}
                 
                 </div>}
         </div>
