2ddb7da2282a363eb07a34b6140af872d25f6226
rifat3790 <mdrifayethossen@gmail.com>
2023-09-23 15:27:26 +0600

    Loging page added

---
+++ src/Pages/Favourites/Favourites.jsx
@@ -0,0 +1,10 @@
+
+const Favourites = () => {
+    return (
+        <div>
+            <h2>This is Favourites</h2>
+        </div>
+    );
+};
+
+export default Favourites;
--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -1,9 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import Favourites from "../Pages/Favourites/Favourites";
+import Login from "../Pages/Login/Login";
 
 const myCreateRoute = createBrowserRouter([
     {
-        
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/favorites",
+                element: <Favourites></Favourites>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            }
+        ]
     }
 ])
 
-export default Route;
+export default myCreateRoute;
